ac4e036bb03bfbc91b6570c3ed755b42dc72ea7a
FaustVX <jodu035@gmail.com>
2021-06-07 03:07:41 +0200

    Added first block

---
+++ src/main/java/fr/faustvx/test_mod_1/block/block1.java
@@ -0,0 +1,31 @@
+package fr.faustvx.test_mod_1.block;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import fr.faustvx.test_mod_1.TestMod1;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class block1
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, TestMod1.MOD_ID);
+
+    public static final RegistryObject<Block> BLOCK1 = BLOCKS.register("block1", block1::create_block);
+
+    private static Block create_block()
+    {
+        LOGGER.info("created block: {}", BLOCK1.getId());
+        return new Block(Block.Properties.of(Material.METAL)
+            .harvestTool(ToolType.PICKAXE)
+            .harvestLevel(2)
+            .noDrops()
+            );
+    }
+}
---
+++ src/main/java/fr/faustvx/test_mod_1/item/block1.java
@@ -0,0 +1,31 @@
+package fr.faustvx.test_mod_1.item;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import fr.faustvx.test_mod_1.TestMod1;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class block1
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TestMod1.MOD_ID);
+
+    public static final RegistryObject<BlockItem> BLOCK1 = ITEMS.register("block1", block1::create_item);
+
+    private static BlockItem create_item()
+    {
+        LOGGER.info("created block_item: {}", BLOCK1.getId());
+        return new BlockItem(fr.faustvx.test_mod_1.block.block1.BLOCK1.get(),
+            new Item.Properties()
+                .tab(ItemGroup.TAB_BUILDING_BLOCKS)
+                .fireResistant()
+            );
+    }
+}
